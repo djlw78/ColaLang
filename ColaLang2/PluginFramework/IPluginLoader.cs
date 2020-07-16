c4742e8b3ad072c68b653b2565db83f74893d008
jwoff78 <jwelshoffical78@gmail.com>
2020-07-15 20:45:08 -0500

    errors >-<

---
+++ ColaLang2/PluginFramework/IPluginLoader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColaLang.PluginFramework
+{
+    public interface IPluginLoader
+    {
+        IReadOnlyList<Plugin> GetPlugins();
+
+        void LoadPlugins();
+
+        void UnloadPlugins();
+    }
+}
